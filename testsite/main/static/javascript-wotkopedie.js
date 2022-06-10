9ede0bdca124fd2b9b3f2169a17bf7d646ab549d
honzav23 <janvaclavik@valachnet.cz>
2022-06-10 22:53:55 +0200

    Equipment clicking functional

--- testsite/main/static/javascript-wotkopedie.js
+++ testsite/main/static/javascript-wotkopedie.js
@@ -1,178 +1,193 @@
 
-let Ikonanabijak = null;
-let Ikonaventilace = null;
-let Ikonabratri = null;
-let Ikonaoptika = null;
-let Ikonabino = null;
-let Ikonazamerovak = null;
-window.nabijak;
-window.nabijeni;
-let nabijak2 = null;
-let nabijeni2 = null;
-let nabijak3 = null;
-let nabijeni3 = null;
-let nabijak4 = null;
-let nabijeni4 = null;
-
-let bases = [];
-let out = [];
-
-window.zamerovani;
-window.zamerovak;
-window.dohled;
-window.binoOp;
-window.bino;
-window.presnost;
-window.Presnost;
-window.IkonanabijakBond;
-window.IkonazamerovakBond;
-window.IkonaventilaceBond;
-window.IkonaoptikaBond;
-
-window.onload = setTimeout(loadVariables, 1000)  //loadVariables();
-
 function loadVariables() {
-    Ikonanabijak = document.getElementById("Ikonanabijak");
-    Ikonaventilace = document.getElementById("Ikonaventilace");
-    Ikonabratri = document.getElementById("Ikonabratri");
-    Ikonazamerovak = document.getElementById("Ikonazamerovak");
-    Ikonaoptika = document.getElementById("Ikonaoptika");
-    Ikonabino = document.getElementById("Ikonabino");
-    nabijak2 = document.getElementById("nabijak2");
-    nabijeni2 = document.getElementById("nabijeni2");
-    nabijak3 = document.getElementById("nabijak3");
-    nabijeni3 = document.getElementById("nabijeni3");
-    nabijak4 = document.getElementById("nabijak4");
-    nabijeni4 = document.getElementById("nabijeni4");
-    bases = [nabijeni.innerHTML, zamerovani.innerHTML, dohled.innerHTML, presnost.innerHTML, dohled.innerHTML];
-    out = [nabijak, zamerovak, binoOp, Presnost, bino];
-    if (nabijeni2 != null) {
-        bases.push(nabijeni2.innerHTML);
-        out.push(nabijak2);
-        if (nabijeni3 != null) {
-            bases.push(nabijeni3.innerHTML);
-            out.push(nabijak3);
-            if (nabijeni4 != null) {
-                bases.push(nabijeni4.innerHTML);
-                out.push(nabijak4);
-            }
-        }
+    let changingPropertiesFields = {    // Properties that can change values (reloading, aim time, accuracy, view)
+        baseReloads: document.getElementsByClassName("baseReloading"),
+        baseAimTime: document.getElementById("baseAimTime").innerText,
+        baseAccuracy: document.getElementById("baseAccuracy").innerText,
+        baseView: parseFloat(document.getElementById("baseView").innerText),
+        reloadingIconChecked: document.getElementById("Ikonanabijak") ? document.getElementById("Ikonanabijak").checked : null, // Might be undefined
+        aimingIconChecked: document.getElementById("Ikonazamerovak").checked,
+        opticsIconChecked: document.getElementById("Ikonaoptika").checked,
+        binoIconChecked: document.getElementById("Ikonabino").checked,
+        ventIconChecked: document.getElementById("Ikonaventilace").checked,
+        biaIconChecked: document.getElementById("Ikonabratri").checked,
+        reloadingBondIcon: $("#IkonanabijakBond"),
+        aimingBondIcon: $("#IkonazamerovakBond"),
+        opticsBondIcon: $("#IkonaoptikaBond"),
+        ventBondIcon: $("#IkonaventilaceBond"),
+        nabijak: document.getElementsByClassName("nabijak"),
+        zamerovak: document.getElementById("zamerovak"),
+        presnost: document.getElementById("nabijak"),
+        dohled: document.getElementById("binoOp"),
+        bino: document.getElementById("bino")
     }
+    return changingPropertiesFields
 }
 
 function changeFilter(originalIcon, position, bondIcon) {
     if (originalIcon.checked) {
         if (bondIcon.checked) {
-            $(".bond img").eq(position).css({WebkitFilter: "grayscale(0) blur(0px)"});
+            $(".bond img").eq(position).css({WebkitFilter: "grayscale(0) blur(0px)"})
         }
         // else {
-        //     $(".bond img").eq(position).css({WebkitFilter: "grayscale(0) blur(1px)"});
+        //     $(".bond img").eq(position).css({WebkitFilter: "grayscale(0) blur(1px)"})
         // }
     }
 }
 
-function calculateForNonCrewEquipment(resultsArray, bondEq) {
-    coeficient = 0.1;
-    if (Ikonanabijak != null) {
-        if (Ikonanabijak.checked) {
-            if (IkonanabijakBond.checked) {
-                coeficient = 0.135;
+/**
+ * Resets bond icons to default state (unchecked, disabled)
+ * @param {*} changingPropertiesFields Object with all bond icons
+ */
+function resetBondIcons(changingPropertiesFields) {
+    if (changingPropertiesFields.reloadingIconChecked != null) {
+        if (!changingPropertiesFields.reloadingIconChecked) {
+            changingPropertiesFields.reloadingBondIcon.prop("checked", false)
+            changingPropertiesFields.reloadingBondIcon.prop("disabled", true)
+            $(".bond img").eq(3).css({WebkitFilter: "grayscale(1) blur(1px)"})
+        }
+    }
+    if (changingPropertiesFields.aimingIconChecked != null) {
+        if (!changingPropertiesFields.aimingIconChecked) {
+            changingPropertiesFields.aimingBondIcon.prop("checked", false)
+            changingPropertiesFields.aimingBondIcon.prop("disabled", true)
+            $(".bond img").eq(2).css({WebkitFilter: "grayscale(1) blur(1px)"})
+        }
+    }
+    if (changingPropertiesFields.opticsIconChecked != null) {
+        if (!changingPropertiesFields.opticsIconChecked) {
+            changingPropertiesFields.opticsBondIcon.prop("checked", false)
+            changingPropertiesFields.opticsBondIcon.prop("disabled", true)
+            $(".bond img").eq(1).css({WebkitFilter: "grayscale(1) blur(1px)"})
+        }
+    }
+    if (changingPropertiesFields.ventIconChecked != null) {
+        if (!changingPropertiesFields.ventIconChecked) {
+            changingPropertiesFields.ventBondIcon.prop("checked", false)
+            changingPropertiesFields.ventBondIcon.prop("disabled", true)
+            $(".bond img").eq(0).css({WebkitFilter: "grayscale(1) blur(1px)"})
+        }
+    }
+}
+
+/**
+ * Calculates the results for non crew equipment (gun rammer, optics, ...)
+ */
+function calculateForNonCrewEquipment(changingPropertiesFields, results) {
+    let reloads, aimTime, view, viewBino
+    [reloads, aimTime, view, viewBino] = results
+    if (changingPropertiesFields.reloadingIconChecked != null) {
+        if (changingPropertiesFields.reloadingIconChecked) {
+            let coeficient = 0.1
+            if (changingPropertiesFields.reloadingBondIcon.prop("checked")) {
+                coeficient = 0.135
             }
-            resultsArray[0] = (resultsArray[0] - (resultsArray[0] * 0.875) / (0.00375 * 100 + 0.5) * coeficient).toFixed(2);
-            bondEq[3].prop("disabled", false);
-            $(".bond img").eq(3).css({WebkitFilter: "grayscale(0) blur(1px)"});
+            for (let i = 0; i < changingPropertiesFields.baseReloads.length; i++) {
+                reloads[i] = (parseFloat(baseReload.innerText) - parseFloat(baseReload.innerText) * coeficient).toFixed(2)
+            }
+            changingPropertiesFields.reloadingBondIcon.prop("disabled", false)
+            $(".bond img").eq(3).css({WebkitFilter: "grayscale(0) blur(1px)"})
         }
     }
-    if (Ikonazamerovak.checked) {
-        if (IkonazamerovakBond.checked) {
-            coeficient = 0.135;
+    if (changingPropertiesFields.aimingIconChecked) {
+        let coeficient = 0.1
+        if (changingPropertiesFields.aimingBondIcon.prop("checked")) {
+            coeficient = 0.135
         }
-        resultsArray[1] = (resultsArray[1] - (resultsArray[1] * 0.875) / (0.00375 * 100 + 0.5) * coeficient).toFixed(2);
-        bondEq[1].prop("disabled", false);
-        $(".bond img").eq(2).css({WebkitFilter: "grayscale(0) blur(1px)"});
+        aimTime = (changingPropertiesFields.baseAimTime - changingPropertiesFields.baseAimTime * coeficient).toFixed(2)
+        changingPropertiesFields.aimingBondIcon.prop("disabled", false)
+        $(".bond img").eq(2).css({WebkitFilter: "grayscale(0) blur(1px)"})
     }
-    if (Ikonaoptika.checked) {
-        if (IkonaoptikaBond.checked) {
-            coeficient = 0.135;
+    if (changingPropertiesFields.opticsIconChecked) {
+        let coeficient = 0.1
+        if (changingPropertiesFields.opticsBondIcon.prop("checked")) {
+            coeficient = 0.135
         }
-        resultsArray[2] = (resultsArray[2] + (resultsArray[2] / 0.875) * (0.00375 * 100 + 0.5) * coeficient).toFixed(0);
-        bondEq[0].prop("disabled", false);
-        $(".bond img").eq(1).css({WebkitFilter: "grayscale(0) blur(1px)"});
+        view = (changingPropertiesFields.baseView + changingPropertiesFields.baseView * coeficient).toFixed(0)
+        changingPropertiesFields.opticsBondIcon.prop("disabled", false)
+        $(".bond img").eq(1).css({WebkitFilter: "grayscale(0) blur(1px)"})
     }
-    if (Ikonabino.checked) {
-        coeficient = 0.25;
-        resultsArray[4] = (resultsArray[4] + (resultsArray[4] / 0.875) * (0.00375 * 100 + 0.5) * coeficient).toFixed(0);
+    if (changingPropertiesFields.binoIconChecked) {
+        let coeficient = 0.25
+        viewBino = (changingPropertiesFields.baseView + changingPropertiesFields.baseView * coeficient).toFixed(0)
     }
-    return resultsArray;
+   return [reloads, aimTime, view, viewBino]
+}
+
+function showCalculatedValues(changingPropertiesFields, results) {
+    let greenColor = "#3eb943"
+    let reloads, aimTime, view, binoView
+    [reloads, aimTime, view, binoView] = results
+    changingPropertiesFields.zamerovak.innerText = aimTime
+    changingPropertiesFields.zamerovak.style.backgroundColor = greenColor
+    changingPropertiesFields.dohled.innerText = view
+    changingPropertiesFields.dohled.style.backgroundColor = greenColor
+    changingPropertiesFields.bino.innerText = binoView
+    changingPropertiesFields.bino.style.backgroundColor = greenColor
+
+    for (let i = 0; i < reloads.length; i++) {
+        changingPropertiesFields.nabijak[i].innerText = reloads[i]
+        changingPropertiesFields.nabijak[i].style.backgroundColor = greenColor
+    }
+    if (changingPropertiesFields.zamerovak.innerText == changingPropertiesFields.baseAimTime) {
+        changingPropertiesFields.zamerovak.style.backgroundColor = "inherit"
+    }
+    if (changingPropertiesFields.dohled.innerText == changingPropertiesFields.baseView) {
+        changingPropertiesFields.dohled.style.backgroundColor = "inherit"
+    }
+    if (changingPropertiesFields.bino.innerText == changingPropertiesFields.baseView) {
+        changingPropertiesFields.bino.style.backgroundColor = "inherit"
+        changingPropertiesFields.bino.innerText = ""
+    }
 }
 
 function calculate() {
-    let levelPosadky = 100;
-    let results = [];
-    bondEq = [$("#IkonaoptikaBond"), $("#IkonazamerovakBond"), $("#IkonaventilaceBond"), $("#IkonanabijakBond")];
-    for (base of bases) {
-        results.push(parseFloat(base));
+    let changingPropertiesFields = loadVariables()
+    let crewLevel = 100
+    let reloadingArr = []
+    for (baseReload of changingPropertiesFields.baseReloads) {
+        reloadingArr.push(parseFloat(baseReload.innerText))
     }
-    if (Ikonanabijak != null) {
-        if (!Ikonanabijak.checked) {
-            bondEq[3].prop("checked", false);
-            bondEq[3].prop("disabled", true);
-            $(".bond img").eq(3).css({WebkitFilter: "grayscale(1) blur(1px)"});
-        }
+    let results = [reloadingArr, changingPropertiesFields.baseAimTime, changingPropertiesFields.baseView, changingPropertiesFields.baseView]
+    let accuracy = changingPropertiesFields.baseAccuracy
+    resetBondIcons(changingPropertiesFields)
+    let iconChecked = changingPropertiesFields.aimingIconChecked || changingPropertiesFields.opticsIconChecked
+                        || changingPropertiesFields.binoIconChecked
+    if (changingPropertiesFields.reloadingIconChecked != null) {
+        iconChecked = iconChecked || changingPropertiesFields.reloadingIconChecked
     }
-    if (!Ikonazamerovak.checked) {
-        bondEq[1].prop("checked", false);
-        bondEq[1].prop("disabled", true);
-        $(".bond img").eq(2).css({WebkitFilter: "grayscale(1) blur(1px)"});
-    }
-    if (!Ikonaoptika.checked) {
-        bondEq[0].prop("checked", false);
-        bondEq[0].prop("disabled", true);
-        $(".bond img").eq(1).css({WebkitFilter: "grayscale(1) blur(1px)"});
-    }
-    if (!Ikonaventilace.checked) {
-        bondEq[2].prop("checked", false);
-        bondEq[2].prop("disabled", true);
-        $(".bond img").eq(0).css({WebkitFilter: "grayscale(1) blur(1px)"});
-    }
-    let iconChecked = Ikonazamerovak.checked || Ikonaoptika.checked || Ikonabino.checked;
-    if (Ikonanabijak != null) {
-        iconChecked = iconChecked || Ikonanabijak;
-    }
-    let crewLevelUp = false;
+    let crewLevelUp = false
     if (iconChecked) {
-        results = calculateForNonCrewEquipment(results, bondEq);
+        results = calculateForNonCrewEquipment(changingPropertiesFields, results)
     }
-    if (Ikonaventilace.checked) {
-        bondEq[2].prop("disabled", false);
-        $(".bond img").eq(0).css({WebkitFilter: "grayscale(0) blur(1px)"});
-        levelPosadky += 5.3;
-        crewLevelUp = true;
+    if (changingPropertiesFields.ventIconChecked) {
+        changingPropertiesFields.ventBondIcon.prop("disabled", false)
+        $(".bond img").eq(0).css({WebkitFilter: "grayscale(0) blur(1px)"})
+        crewLevel += 5.3
+        crewLevelUp = true
         if (IkonaventilaceBond.checked) {
-            levelPosadky += 3.3;
+            crewLevel += 3.3
         }
     }
     if (Ikonabratri.checked) {
-        levelPosadky += 5.3;
-        crewLevelUp = true;
+        crewLevel += 5.3
+        crewLevelUp = true
     }
     if (crewLevelUp) {
-        for (let i = 0; i < bases.length; i++) {
-            if (i == 2 || i == 4) {
-                results[i] = ((results[i] / 0.875) * (0.00375 * levelPosadky + 0.5)).toFixed(0);
-            }
-            else {
-                results[i] = ((results[i] * 0.875) / (0.00375 * levelPosadky + 0.5)).toFixed(2);
-            }
-        }
+            results[1] = ((results[1] * 0.875) / (0.00375 * crewLevel + 0.5)).toFixed(2)
+            results[2] = ((results[2] / 0.875) * (0.00375 * crewLevel + 0.5)).toFixed(0)
+            results[3] = ((results[3] / 0.875) * (0.00375 * crewLevel + 0.5)).toFixed(0)
+            accuracy = ((parseFloat(accuracy) * 0.875) / (0.00375 * crewLevel + 0.5)).toFixed(2)
+            results[0] = results[0].map(elem =>((elem * 0.875) / (0.00375 * crewLevel + 0.5)).toFixed(2))
     }
-    for (let i = 0; i < bases.length; i++) {
-        out[i].innerHTML = results[i];
-        if (results[i] != bases[i]) {
-            out[i].style.backgroundColor = "#3eb943";
-        }
-        else {
-            out[i].style.backgroundColor = "inherit";
-        }
-    }
+    showCalculatedValues(changingPropertiesFields, results)
+    // for (let i = 0; i < bases.length; i++) {
+    //     out[i].innerHTML = results[i]
+    //     if (results[i] != bases[i]) {
+    //         out[i].style.backgroundColor = "#3eb943"
+    //     }
+    //     else {
+    //         out[i].style.backgroundColor = "inherit"
+    //     }
+    // }
 }
